9673967b069f27aaa7ebc9941dbb563522254281
yusuparipin30 <yusuparipin30@gmail.com>
2022-11-06 21:28:11 +0700

    first commit

--- src/components/EditProduct.js
+++ src/components/EditProduct.js
@@ -1,26 +1,26 @@
-import {useState,useEffect} from "react";
-import {useNavigate,useParams} from "react-router-dom";
-
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+ 
 const EditProduct = () => {
     const [title, setTitle] = useState('');
     const [price, setPrice] = useState('');
-    const navigate = useNavigate();
+    const history = useNavigate();
     const { id } = useParams();
-
-    useEffect(() =>{
+ 
+    useEffect(() => {
         getProductById();
-    },[]);
-
+    }, []);
+ 
     const getProductById = async() => {
         const response = await fetch(`http://localhost:8080/products/${id}`);
         const data = await response.json();
         setTitle(data.title);
         setPrice(data.price);
     }
-
+ 
     const updateProduct = async(e) => {
         e.preventDefault();
-        const product = {title, price};
+        const product = { title, price };
         await fetch(`http://localhost:8080/products/${id}`,{
             method: "PUT",
             body: JSON.stringify(product),
@@ -28,33 +28,34 @@ const EditProduct = () => {
                 'Content-Type': 'application/json'
             }
         });
-        navigate.push("/");
+        history.push("/");
     }
+ 
     return (
         <div>
             <form onSubmit={updateProduct}>
                 <div className="field">
-                    <label className="label">Title</label>
-                    <div className="control">
-                        <input className="input" value={title} onChange={(e) => setTitle(e.target.value)} type="text" placeholder="Title"/>
-                    </div>
+                <label className="label">Title</label>
+                <div className="control">
+                    <input className="input" value={title} onChange={(e) => setTitle(e.target.value)} type="text" placeholder="Title" />
                 </div>
-
-                <div className="field">
-                    <label className="label">Price</label>
-                    <div className="control">
-                        <input className="input" value={price} onChange={(e) => setPrice(e.target.value)} type="text" placeholder="Price"/>
-                    </div>
                 </div>
-
+ 
                 <div className="field">
-                    <div className="control">
-                        <button className="button is-primary">Update</button>
-                    </div>
+                <label className="label">Price</label>
+                <div className="control">
+                    <input className="input" value={price} onChange={(e) => setPrice(e.target.value)} type="text" placeholder="Price" />
+                </div>
+                </div>
+         
+                <div className="field">
+                <div className="control">
+                    <button className="button is-primary">Update</button>
+                </div>
                 </div>
             </form>
         </div>
     )
 }
-
+ 
 export default EditProduct
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -27,28 +27,29 @@ const ProductList = () => {
     return (
         <div>
             <Link to="/add" className="button is-primary mt-5">Add New</Link>
-            <table className="table is-striped">
+            <table className="table is-striped is-fullwidth">
                 <thead>
                     <tr>
                         <th>No</th>
-                        <th>Title</th>
+                        <th>Product</th>
                         <th>Price</th>
                         <th>Actions</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {products.map((product,index) => (
-                    <tr key={product.id}>
-                         <td>{index + 1}</td>
-                         <td>{product.title}</td>
-                         <td>{product.price}</td>
-                         <td>
-                            <Link to={`/edit/${product.id}`} className="button is-small is-info">Edit</Link>
-                            <button onClick={() => deleteProduct(product.id)} className="button is-small is-danger">Delete</button>
-                         </td>
-                     </tr>
+                    {products.map((product, index) => (
+                        <tr key={product.id}>
+                            <td>{ index + 1 }</td>
+                            <td>{ product.title }</td>
+                            <td>{ product.price }</td>
+                            <td>
+                                <Link to={`/edit/${product.id}`} className="button is-small is-info">Edit</Link>
+                                <button onClick={() => deleteProduct(product.id)} className="button is-small is-danger">Delete</button>
+                            </td>
+                        </tr>
                     ))}
-                   
+                     
+ 
                 </tbody>
             </table>
         </div>
