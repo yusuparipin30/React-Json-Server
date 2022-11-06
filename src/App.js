23bde0478f701c2e132a00cdf677cb2fc06ae0fd
yusuparipin30 <yusuparipin30@gmail.com>
2022-11-06 20:06:42 +0700

    error push not function

--- src/App.js
+++ src/App.js
@@ -1,45 +1,27 @@
-
-import { useState } from "react"
-import ProductList from "./components/ProductList";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import About from "./components/About";
-import Contact from "./components/Contact";
+import ProductList from "./components/ProductList";
+import AddProduct from "./components/AddProduct";
+import EditProduct from "./components/EditProduct";
  
 function App() {
-  const [products, setProducts] = useState([
-    {id: 1, title: 'Product 1', price: 899},
-    {id: 2, title: 'Product 2', price: 982},
-    {id: 3, title: 'Product 3', price: 322},
-    {id: 4, title: 'Product 4', price: 763},
-    {id: 5, title: 'Product 5', price: 389}
-  ]);
  
-  const deleteProduct = (productId) => {
-    const newProducts = products.filter(product => product.id !== productId);
-    setProducts(newProducts);
-  }
- 
-          //di dalam router gunakan fungsi Routes
-          //didalam Routes gunakan route yang di instal dari react router dom
-          //di dalam route terdapat atribut path yaitu url
-          //panggil komponen pada saat mengunjungi slash
   return (
-    <div>
-      <Router>
-        <Routes>
+    <div className="container">
+      <div className="columns">
+        <div className="column is-half is-offset-one-quarter">
+          <Router>
+            <Routes>
 
-          <Route exact path="/" element={<ProductList/>} products={ products } deleteProduct={ deleteProduct }/>
-          
-          <Route path="/about" element={<About/>} />
-            
-          
+              <Route exact path="/" element={<ProductList/>} />
+              
+              <Route path="/add" element={<AddProduct/>} />
 
-          <Route path="/contact" element={<Contact/>} />
-    
-          
-
-        </Routes>
-      </Router>
+              <Route path="/edit/:id" element={<EditProduct/>} />
+                
+            </Routes>
+          </Router>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
+//import ReactDOM from 'react-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import "bulma/css/bulma.css";
+ 
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
